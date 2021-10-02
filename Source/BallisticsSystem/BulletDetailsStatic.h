6c4005d68b45e091e59619390e72c91255cdaea4
Joy Goldar <joy211191@gmail.com>
2021-10-02 12:27:35 +0200

    Initial Push

---
+++ Source/BallisticsSystem/BulletDetailsStatic.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BulletDetailsStatic.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLISTICSSYSTEM_API UBulletDetailsStatic : public UObject
+{
+	GENERATED_BODY()
+	
+};
